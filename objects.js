c9d8ead21814ce83e319904aaf37df77bf0a8a39
Amanpreet Chatha <50677671+amanpreetchatha@users.noreply.github.com>
2025-09-02 20:16:10 -0500

    created object for a food item

---
+++ objects.js
@@ -0,0 +1,19 @@
+var foodItem = {
+    name : "Doritos",
+    flavor : "Nacho cheese",
+    servingSizeInGrams : 28,
+    caloriesPerServing : 150,
+    totalFat : 8,
+    saturatedFat : 1,
+    transFat : 0,
+    cholestrol : 0,
+    sodium : 0.170,
+    totalCarbs : 17,
+    fiber : 1,
+    totalSugar : 0,
+    protein : 2,
+    vitaminD : 0,
+    calcium : 0.050,
+    iron : 0.003,
+    potassium : 0.100
+}
