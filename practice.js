c32245a99bd123a416cb7695d81eda842bfce8fa
amanpreetchatha <amanpreetchatha99@gmail.com>
2025-09-03 22:03:15 -0400

    file updated

---
+++ practice.js
@@ -0,0 +1,34 @@
+//let pi=Math.PI;
+//let random=Math.random();
+
+var userName = "Amanpreet Singh Chatha";
+var array;
+array=userName.split(" ");
+let i = 0;
+console.log(array);
+while(i<userName.length){
+    /*if(Math.round(Math.random())==1){
+        console.log("Found 1");
+    }else{
+        console.log("found nothing");
+    }*/
+    //console.log("Square root of:",i," is :",Math.sqrt(i));
+    //console.log("Cube root of:",i," is :",Math.cbrt(i));
+    //console.log(userName[i]);
+    //array.push("aa");
+    
+    console.log(userName.lastIndexOf("a"));
+    console.log(userName.indexOf('a'))
+    
+    //array.pop();
+    i++;
+}
+console.log(array);
+
+
+
+
+
+
+
+
