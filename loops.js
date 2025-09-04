d969f3c044ffbac1e2cab1bc529de50b32acc89d
amanpreetchatha <amanpreetchatha99@gmail.com>
2025-09-03 21:50:07 -0400

    learning loops

---
+++ loops.js
@@ -0,0 +1,38 @@
+/*var x=0;
+var y=0;
+
+// loop lables and break/continue statements.
+xloop: while(x<10){
+    yloop: while(y<10){
+        console.log("You are in y loop where y =",y);
+        y++;
+        if(x==5) {
+            continue xloop;
+            
+        }
+        console.log("hi")
+        
+    }
+    console.log("You are in x loop where x =",x);
+    x++;
+    y=0;
+}
+*/
+
+
+/*let x=[];
+x[5]="apple";
+for(let item in x){
+    console.log(item);
+}*/
+
+//console.log(x.prototype);
+//console.log(Array.prototype)
+/*let x= ["apple","squid","speaker"]
+let getIterator= x[Symbol.iterator]();
+console.log(getIterator)
+*/
+
+
+
+
