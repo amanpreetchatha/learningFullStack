44aa3951827f9ffd847cfd5664a8c4588ad24129
amanpreetchatha <amanpreetchatha99@gmail.com>
2025-09-05 14:58:38 -0400

    learning arrays

---
+++ arrays&objects.js
@@ -0,0 +1,32 @@
+let myObject = {
+    firstName: "Aman",
+    lastName: "Chatha",
+    age: 101
+}
+//let array=Object.entries(myObject);
+
+let array = [2022, "volvo", myObject, 22.43, "jjajf", 322];
+let array1 = new Array(2022, "volvo", myObject);
+
+console.clear();
+//console.log(array1.at(-10));
+/*
+array.push("HI");
+array.unshift("Truck");
+console.log(array);
+array.pop();
+array.shift();
+console.log(array);
+
+
+//array.splice(-5,5, 2025, 2026, myObject);
+array.splice(0,2);
+console.log(array);
+
+
+/*
+for( let [key,value] of array){
+    console.log(key,value)
+}
+*/
+
